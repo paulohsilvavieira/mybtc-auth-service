7c82c17eeaa7d09e049a86a0a63d1d23895cfc8a
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-06-11 19:26:22 -0300

    test: create mockDatabase

---
+++ test/mocks/database.ts
@@ -0,0 +1,29 @@
+import path from 'node:path';
+import { IMemoryDb, newDb } from 'pg-mem';
+import { DataSource } from 'typeorm';
+
+export const makeFakeDb = async (
+  entities?: any[],
+): Promise<{ db: IMemoryDb; connection: DataSource }> => {
+  const db = newDb();
+
+  db.public.registerFunction({
+    implementation: () => 'test',
+    name: 'current_database',
+  });
+
+  db.public.registerFunction({
+    implementation: () => 'test',
+    name: 'version',
+  });
+  const databaseConnection = db.adapters.createTypeormDataSource({
+    type: 'postgres',
+    entities: entities ?? [__dirname + '../entities/index.{ts,js}'],
+  });
+
+  const connection: DataSource = await databaseConnection.initialize();
+
+  await connection.synchronize();
+
+  return { db, connection };
+};
