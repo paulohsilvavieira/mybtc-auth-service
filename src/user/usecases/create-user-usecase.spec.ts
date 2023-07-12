ecdb7e1961ec5db61e83c68f0f55d6a1cd43b30f
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-07-12 19:27:15 -0300

    test: create mocks to use on usecase

--- src/user/usecases/create-user-usecase.spec.ts
+++ src/user/usecases/create-user-usecase.spec.ts
@@ -1,3 +1,21 @@
+import { MockProxy, mock } from 'jest-mock-extended';
+import { CreateUserUseCase } from './create-user-usecase';
+import { UserRepoProtocol } from '../protocols/repositories';
+import { mockCreateUserInput, throwError } from '@tests/utils/mocks';
+
 describe('CreateUserUseCase', () => {
+  let userRepositoryMock: MockProxy<UserRepoProtocol>;
+
+  let sut: CreateUserUseCase;
+  beforeAll(() => {
+    userRepositoryMock = mock();
+    userRepositoryMock.createUser.mockResolvedValue({
+      success: true,
+    });
+  });
+
+  beforeEach(() => {
+    sut = new CreateUserUseCase(userRepositoryMock);
+  });
   test('', () => {});
 });
