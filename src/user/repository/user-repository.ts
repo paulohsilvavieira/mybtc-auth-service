27c6747bc4dd67ee3fe91d22e7a45fcfc2e79ae2
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-07-13 08:02:33 -0300

    feat: create user repository

---
+++ src/user/repository/user-repository.ts
@@ -0,0 +1,40 @@
+import { UserEntity } from '@entities/UserEntity';
+import {
+  CreateUserRepoInput,
+  CreateUserRepoOutput,
+  UserRepoProtocol,
+} from '../protocols/repositories';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository implements UserRepoProtocol {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userTypeOrmRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(params: CreateUserRepoInput): Promise<CreateUserRepoOutput> {
+    const { authenticationId, lastName, firstName, phoneNumber } = params;
+    const saveUserResult = await this.userTypeOrmRepository.update(
+      {
+        authenticationId,
+      },
+      {
+        firstName,
+        lastName,
+        phoneNumber,
+      },
+    );
+    if (saveUserResult.affected > 0) {
+      return {
+        success: true,
+      };
+    }
+
+    return {
+      success: false,
+      error: 'Error on Save User Info',
+    };
+  }
+}
