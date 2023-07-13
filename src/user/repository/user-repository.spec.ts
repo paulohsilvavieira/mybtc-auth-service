a5950319618ded0f6aaf19c96ff334aafb307f43
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-07-13 08:03:46 -0300

    test: create test user-repository

---
+++ src/user/repository/user-repository.spec.ts
@@ -0,0 +1,61 @@
+import { Repository } from 'typeorm';
+import { UserEntity } from '@entities/UserEntity';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserRepository } from './user-repository';
+import { mockCreateUserInput } from '@tests/utils/mocks';
+
+describe('AuthenticationRepository', () => {
+  let sut: UserRepository;
+  let authRepositoryMock: Repository<UserEntity>;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [],
+      providers: [
+        UserRepository,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    sut = moduleRef.get<UserRepository>(UserRepository);
+    authRepositoryMock = moduleRef.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+  });
+
+  test('should return success: true, if save user info', async () => {
+    jest.spyOn(authRepositoryMock, 'update').mockResolvedValue({
+      affected: 1,
+      raw: 'any',
+      generatedMaps: {} as any,
+    });
+
+    const result = await sut.createUser(mockCreateUserInput());
+
+    expect(result).toEqual({
+      success: true,
+    });
+  });
+
+  test('should return success: false, if dont save user info', async () => {
+    jest.spyOn(authRepositoryMock, 'update').mockResolvedValue({
+      affected: 0,
+      raw: 'any',
+      generatedMaps: {} as any,
+    });
+    const result = await sut.createUser(mockCreateUserInput());
+
+    expect(result).toEqual({
+      success: false,
+      error: 'Error on Save User Info',
+    });
+  });
+});
