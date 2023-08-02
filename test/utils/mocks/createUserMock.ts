887da73031a9d1b823c42bc82060ba2395f3d114
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-08-02 13:37:49 -0300

    chore: create mocks

--- test/utils/mocks/createUserMock.ts
+++ test/utils/mocks/createUserMock.ts
@@ -1,5 +1,9 @@
 import { faker } from '@faker-js/faker';
-import { CreateUserUsecaseInput } from 'src/user/protocols/usecases';
+import {
+  CreateUserUsecaseInput,
+  SaveAddressUserUsecaseInput,
+  SaveDocumentsUserUsecaseInput,
+} from 'src/user/protocols/usecases';
 
 export const mockCreateUserInput = (): CreateUserUsecaseInput => ({
   firstName: faker.person.firstName(),
@@ -7,3 +11,20 @@ export const mockCreateUserInput = (): CreateUserUsecaseInput => ({
   phoneNumber: faker.phone.number(),
   authenticationId: faker.string.uuid(),
 });
+
+export const mockSaveAddressUserInput = (): SaveAddressUserUsecaseInput => ({
+  address: faker.location.streetAddress(),
+  state: faker.location.state(),
+  country: faker.location.country(),
+  proofAddress: faker.image.urlLoremFlickr(),
+  authenticationId: faker.string.uuid(),
+});
+
+export const mockSaveDocumentsUserInput =
+  (): SaveDocumentsUserUsecaseInput => ({
+    document: faker.string.alpha(),
+    typeDocument: faker.string.alpha(),
+    proofDocumentBack: faker.image.urlLoremFlickr(),
+    proofDocumentFront: faker.image.urlLoremFlickr(),
+    authenticationId: faker.string.uuid(),
+  });
