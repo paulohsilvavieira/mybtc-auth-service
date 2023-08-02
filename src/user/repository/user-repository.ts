8be883cf1aaf8842941ea61c921729fb90ef3ede
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-08-02 13:23:29 -0300

    feat: implements saveAddress and saveDocuments

--- src/user/repository/user-repository.ts
+++ src/user/repository/user-repository.ts
@@ -2,6 +2,10 @@ import { UserEntity } from '../../database/entities/UserEntity';
 import {
   CreateUserRepoInput,
   CreateUserRepoOutput,
+  SaveAddressInfoRepoInput,
+  SaveAddressInfoRepoOutput,
+  SaveUserDocumentsInfoRepoInput,
+  SaveUserDocumentsInfoRepoOutput,
   UserRepoProtocol,
 } from '../protocols/repositories';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,6 +18,7 @@ export class UserRepository implements UserRepoProtocol {
     @InjectRepository(UserEntity)
     private readonly userTypeOrmRepository: Repository<UserEntity>,
   ) {}
+
   async createUser(params: CreateUserRepoInput): Promise<CreateUserRepoOutput> {
     const { authenticationId, lastName, firstName, phoneNumber } = params;
     const saveUserResult = await this.userTypeOrmRepository.update(
@@ -35,6 +40,65 @@ export class UserRepository implements UserRepoProtocol {
     return {
       success: false,
       error: 'Error on Save User Info',
+    };
+  }
+
+  async saveAddressUser(
+    params: SaveAddressInfoRepoInput,
+  ): Promise<SaveAddressInfoRepoOutput> {
+    const { authenticationId, address, proofAddress, state, country } = params;
+    const saveUserResult = await this.userTypeOrmRepository.update(
+      {
+        authenticationId,
+      },
+      {
+        address,
+        proofAddress,
+        state,
+        country,
+      },
+    );
+    if (saveUserResult.affected > 0) {
+      return {
+        success: true,
+      };
+    }
+
+    return {
+      success: false,
+      error: 'Error on Save Address User Info',
+    };
+  }
+  async saveDocuments(
+    params: SaveUserDocumentsInfoRepoInput,
+  ): Promise<SaveUserDocumentsInfoRepoOutput> {
+    const {
+      authenticationId,
+      document,
+      typeDocument,
+      proofDocumentBack,
+      proofDocumentFront,
+    } = params;
+    const saveUserResult = await this.userTypeOrmRepository.update(
+      {
+        authenticationId,
+      },
+      {
+        document,
+        typeDocument,
+        proofDocumentFront,
+        proofDocumentBack,
+      },
+    );
+    if (saveUserResult.affected > 0) {
+      return {
+        success: true,
+      };
+    }
+
+    return {
+      success: false,
+      error: 'Error on Save Documents User Info',
     };
   }
 }
