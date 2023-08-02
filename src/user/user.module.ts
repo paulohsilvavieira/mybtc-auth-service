593d4377a4c7ee3ad30dc7d69361ae89e1fbeba0
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-08-02 13:39:05 -0300

    chore: create usermodule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,44 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/database/entities';
+import { UserRepoProtocol } from './protocols/repositories';
+import { UserRepository } from './repository/user-repository';
+import {
+  CreateUserProtocol,
+  SaveAddressUserProtocol,
+  SaveDocumentsUserProtocol,
+} from './protocols/usecases';
+import { CreateUserUseCase } from './usecases/create-user-usecase';
+import { SaveAddressUserUseCase } from './usecases/save-address-info-user-usecase';
+import { SaveDocumentsUserUseCase } from './usecases/save-documents-user-usecase';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [
+    {
+      provide: UserRepoProtocol,
+      useClass: UserRepository,
+    },
+
+    {
+      provide: CreateUserProtocol,
+      useClass: CreateUserUseCase,
+    },
+    {
+      provide: SaveAddressUserProtocol,
+      useClass: SaveAddressUserUseCase,
+    },
+    {
+      provide: SaveDocumentsUserProtocol,
+      useClass: SaveDocumentsUserUseCase,
+    },
+  ],
+
+  exports: [
+    SaveDocumentsUserProtocol,
+    SaveAddressUserProtocol,
+    CreateUserProtocol,
+    UserRepoProtocol,
+  ],
+})
 export class UserModule {}
