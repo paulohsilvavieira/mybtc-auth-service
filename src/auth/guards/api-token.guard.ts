e9bf2c46b74ed53ed188bae4be227a2d8506d655
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-08-22 09:01:20 -0300

    test: create testes to api-token guard

--- src/auth/guards/api-token.guard.ts
+++ src/auth/guards/api-token.guard.ts
@@ -1,11 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { Request } from 'express';
-import { AuthRepoProtocol } from '../protocols/repository';
 import { JwtProtocol } from '../protocols/cryptography/jwt';
 
 @Injectable()
 export class ApiTokenGuard implements CanActivate {
-  constructor(private readonly jwt: JwtProtocol) {}
+  constructor(
+    @Inject(JwtProtocol)
+    private readonly jwt: JwtProtocol,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const authToken = request.get('Authorization').split(' ')[1];
