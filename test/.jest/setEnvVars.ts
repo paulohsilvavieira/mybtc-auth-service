700191b94a748baf6af73705f8623f5f060c2a10
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-12-29 22:48:18 -0300

    chore: update setEnvVars

--- test/.jest/setEnvVars.ts
+++ test/.jest/setEnvVars.ts
@@ -1,4 +1,9 @@
-process.env.AMQP_URI_CONNECTION = process.env.AMQP_URI_CONNECTION
+if (process.env.AMQP_URI_CONNECTION) {
+  process.env.AMQP_URI_CONNECTION = process.env.AMQP_URI_CONNECTION
+
+} else {
+  process.env.AMQP_URI_CONNECTION = 'amqp://user:password@127.0.0.1:35672'
+}
 process.env.EXCHANGE_NAME_MAILER_SERVICE = 'my_bitcoin.mailer'
 process.env.ROUTING_KEY_MAILER_SERVICE = 'mail.recover_password'
 process.env.SECRET_JWT = 'dummy'
